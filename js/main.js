b2d84775bb27525c4ef6848bd6f0221be8653bea
n0g4y0 <42777385+n0g4y0@users.noreply.github.com>
2024-01-18 00:12:06 -0400

    function to know who win

--- js/main.js
+++ js/main.js
@@ -2,3 +2,16 @@ function getComputerChoice(){
     const OPTIONS = ['scissor', 'paper', 'rock'];
     return OPTIONS[Math.floor(Math.random() * 3)];
 }
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+
+    const MOVES = {'paper': 'rock', 'scissor': 'paper', 'rock': 'scissor'}
+    if(playerSelection === computerSelection){
+        return `It's Draw`
+    }
+    if(MOVES[playerSelection] === computerSelection){
+        return 'P';
+    }else{
+        return 'C';
+    }
+}
