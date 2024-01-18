270c7083ceb904c3f258460ac8b78fe1cfc36695
n0g4y0 <42777385+n0g4y0@users.noreply.github.com>
2024-01-18 00:12:41 -0400

    main function, best three win

--- js/main.js
+++ js/main.js
@@ -2,6 +2,35 @@ function getComputerChoice(){
     const OPTIONS = ['scissor', 'paper', 'rock'];
     return OPTIONS[Math.floor(Math.random() * 3)];
 }
+
+function game(){
+    let answerPlayer = '';
+    let computerSeleccion = '';
+    let winner = '';
+    const ROUNDS = 3;
+    let count = 1;
+    let playerPoints = 0;
+    let computerPoints = 0;
+
+
+
+    while(count <= ROUNDS){
+    
+        answerPlayer = prompt("Paper, Rock or Scissor ? (put your answer in lowercase, please): \n");
+        computerSeleccion = getComputerChoice();
+        console.log(" the computer responds : " +computerSeleccion + "\n");
+        winner = playRound(answerPlayer,computerSeleccion);
+        winner === 'P'? playerPoints++: winner === 'C'?computerPoints++: console.log('Draw');
+        count++;
+
+    }
+    return playerPoints > computerPoints? 'player win!': 'computer win';
+    
+    
+}
+
+function playRound(playerSelection, computerSelection) {
+
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
 
